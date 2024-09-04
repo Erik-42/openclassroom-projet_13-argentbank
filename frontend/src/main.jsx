a3750d525c9b559e17f1607b8b8dd29c89a39e63
Erik-42 <mesen.erik@gmail.com>
2024-09-04 12:31:53 +0200

    Test Speed Insight Vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { store } from "./store/store.js";
 import { Provider } from "react-redux";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Provider store={store}>
 			<App />
+			<SpeedInsights />
 		</Provider>
 	</StrictMode>
 );
