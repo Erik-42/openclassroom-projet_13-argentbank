942764e29ef9c04c76b8fd008b978a142e702f12
Erik-42 <mesen.erik@gmail.com>
2024-09-04 12:42:15 +0200

    Maj App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Header from "./layouts/header/header";
 import Footer from "./layouts/footer/footer";
 import Error404 from "./pages/error404/error404";
 import ConnectedRoute from "./components/connectedRoutes/connectedRoute";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
 	return (
@@ -26,6 +27,7 @@ function App() {
 				<Route path="*" element={<Error404></Error404>} />
 			</Routes>
 			<Footer></Footer>
+			<SpeedInsights />
 		</BrowserRouter>
 	);
 }
